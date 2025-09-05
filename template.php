33fbb29e38fd1ac6931d2c2cc8d8f261ed33cf93
knightsri <sbolisetty@i-viewnow.com>
2025-09-05 18:59:07 -0500

    Try-On Jewelry Application

--- template.php
+++ template.php
@@ -7,12 +7,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jewelry Try-On Application</title>
+    <title>Try-On Jewelry Application</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="container">
-        <h1>Jewelry Try-On Application</h1>
+        <h1>Try-On Jewelry Application</h1>
 
         <?php if ($state === 'form'): ?>
             <!-- Upload form -->
